0b1fc6c70a8a76152bccbba47ef5ad705cfeea8b
André Arruda <andrearruda82@gmail.com>
2017-02-24 17:31:18 -0300

    navigation + breadcrumb

--- config/autoload/navigation.global.php
+++ config/autoload/navigation.global.php
@@ -5,23 +5,33 @@ return [
             'home' => [
                 'label' => 'Home',
                 'route' => 'home',
-                'icon' => 'home',
+                'action' => 'index',
+                'icon' => 'home'
             ],
-            'campaign' => [
-                'label' => 'Campanhas',
-                'route' => 'campaign',
+            'registration' => [
+                'label' => 'Cadastros',
+                'uri' => 'javascript:void(0);',
                 'icon' => 'assignment',
                 'pages' => [
-                    'add' => [
-                        'label' => 'Adicionar',
-                        'route' => 'campaign/default',
-                        'action' => 'add',
+                    'campaign' => [
+                        'label' => 'Campanhas',
+                        'route' => 'campaign',
+                        'action' => 'index',
+                        'pages' => [
+                            'add' => [
+                                'label' => 'Adicionar',
+                                'route' => 'campaign/default',
+                                'action' => 'add',
+                                'show_in_menu' => false
+                            ],
+                            'edit' => [
+                                'label' => 'Editar',
+                                'route' => 'campaign/default',
+                                'action' => 'edit',
+                                'show_in_menu' => false
+                            ]
+                        ]
                     ],
-                    'edit' => [
-                        'label' => 'Editar',
-                        'route' => 'campaign/default',
-                        'action' => 'edit',
-                    ]
                 ]
             ],
         ]
