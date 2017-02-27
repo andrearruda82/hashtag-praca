a6ac35784a312441ba4dcbad8fbb6ddd8a06ffd3
Andre Arruda <andrearruda82@gmail.com>
2017-02-26 21:57:00 -0300

    ajustes no headLink + headScript + inlineScript + Calendar nos inputs de datas

--- public/assets/js/pages/campaign.js
+++ public/assets/js/pages/campaign.js
@@ -4,6 +4,8 @@ $(function () {
         weekStart: 0,
         format: 'DD/MM/YYYY',
         time: false
+    }).on('change', function(e, date) {
+        $(this).parent().addClass('focused');
     });
 
     $('#period_start').bootstrapMaterialDatePicker({
@@ -13,5 +15,6 @@ $(function () {
         time: false
     }).on('change', function(e, date) {
         $('#period_final').bootstrapMaterialDatePicker('setMinDate', date);
+        $(this).parent().addClass('focused');
     })
 });
